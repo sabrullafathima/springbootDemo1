123755f09c9e3fed16369063faa7d56ab65ae4a3
fathima-mdosl <fathima@mydigitaloffice.ca>
2022-08-25 18:16:47 +0530

    modified application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 	ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
 	run.close();
+	//change 2
 	}
 
 }
